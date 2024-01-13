507bb993aa17f4aecf1b5a87fb0786b4f0d5732b
StjepanPetrovic <stjepanpetrovic11@gmail.com>
2024-01-13 18:25:58 +0100

    wip

--- WorkerProcess.py
+++ WorkerProcess.py
@@ -1,26 +1,146 @@
+import json
 import socket
+import time
 from multiprocessing import Process
 
 
 class WorkerProcess:
-    def __init__(self, port, clock, time_to_work):
-        self.port = port
+    def __init__(self, id_process, clock, times_to_critical_section, number_of_other_processes):
+        self.id = id_process
         self.clock = clock
-        self.time_to_work = time_to_work
+        self.times_to_critical_section = times_to_critical_section
+        self.number_of_other_processes = number_of_other_processes
         self.socket = self.create_server_socket()
+        self.need_to_crtical_section = False
+
+        self.list_of_requests = []
+        self.list_of_responses = []
+
         self.process = Process(target=self.work)
+        time.sleep(2)
         self.process.start()
+
+    def critical_section(self):
+        self.send_request_messages_to_all_processes()
+
+        self.list_of_requests.append([self.id, self.clock])
+
+        while (len(self.list_of_responses) < self.number_of_other_processes) or self.list_of_requests[0][0] != self.id:
+
+            print(f'P{self.id} ceka na odgovore')
+
+            data, sender_process_id = self.receive_message()
+
+            if data['type'] == 'request':
+
+                print(f'P{self.id} primio Z({sender_process_id}, {data["clock"]}) od P{sender_process_id}')
+
+                self.update_clock()
+
+                self.list_of_responses.append([sender_process_id, data['clock']])
+
+                if sender_process_id == 3:
+                    self.send_message(sender_process_id, 'response')
+
+            elif data['type'] == 'release':
+
+                print(f'P{self.id} primio I({sender_process_id}, {data["clock"]}) od P{sender_process_id}')
+
+                self.list_of_requests.remove([sender_process_id, data['clock']])
+
+            else:
+
+                print(f'P{self.id} primio O({sender_process_id}, {data["clock"]}) od P{sender_process_id}')
+
+                self.update_clock()
+
+        self.list_of_requests.pop(0)
+        self.list_of_responses = []
+
+        print(f'P{self.id} usao u kriticni odsjecak')
+
+        time.sleep(3)
+
+        self.need_to_crtical_section = False
+
+        print(f'P{self.id} izasao iz kriticnog odsjecka')
+
+        self.send_release_messages_to_all_processes()
 
     def work(self):
         while True:
-            data, _ = self.socket.recvfrom(4096)
+            if any(self.clock >= time_to_critical_section for time_to_critical_section in
+                   self.times_to_critical_section):
+                self.need_to_crtical_section = True
+
+            while self.need_to_crtical_section:
+                self.critical_section()
+
+            time.sleep(1)
+
+            self.clock += 1
+
+            data, sender_process_id = self.receive_message()
+
+            if data['type'] == 'request':
+                print(f'P{self.id} primio Z({sender_process_id}, {data["clock"]}) od P{sender_process_id}')
+
+                self.update_clock()
+
+                self.list_of_requests.append([sender_process_id, data['clock']])
+
+                self.send_message(sender_process_id, 'response')
+            elif data['type'] == 'release':
+                print(f'P{self.id} primio I({sender_process_id}, {data["clock"]}) od P{sender_process_id}')
+
+                self.list_of_requests.remove([sender_process_id, data['clock']])
 
     def join(self):
         self.process.join()
 
     def create_server_socket(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_address = ('localhost', self.port)
+        server_address = ('localhost', 8000 + self.id)
         server_socket.bind(server_address)
 
         return server_socket
+
+    def send_message(self, process, response_type):
+        address = ('localhost', 8000 + process)
+
+        response = {
+            'type': response_type,
+            'clock': self.clock
+        }
+
+        response = json.dumps(response)
+
+        self.socket.sendto(response.encode(), address)
+
+    def receive_message(self):
+        data, address = self.socket.recvfrom(4096)
+
+        data = json.loads(data.decode())
+
+        _, port = address
+
+        sender_process_id = port % 10
+
+        return data, sender_process_id
+
+    def send_request_messages_to_all_processes(self):
+        for i in range(self.number_of_other_processes + 1):
+            if (i + 1) != self.id:
+                self.send_message(i + 1, 'request')
+                print(f'P{self.id} poslao Z({self.id}, {self.clock}) k P{i + 1}')
+
+    def send_release_messages_to_all_processes(self):
+        for i in range(self.number_of_other_processes + 1):
+            if (i + 1) != self.id:
+                self.send_message(i + 1, 'release')
+                print(f'P{self.id} poslao I({self.id}, {self.clock}) k P{i + 1}')
+
+    def update_clock(self):
+        self.clock += 1
+        # print("Dogadaj " + self.id)
+        print(f"T({self.id}) = {self.clock}")
--- main.py
+++ main.py
@@ -2,33 +2,60 @@ import sys
 
 from WorkerProcess import WorkerProcess
 
-if __name__ == "__main__":
 
-    base_port = 8000
-
-    numbers = []
-    temp = []
+def read_input_arguments():
+    times = []
+    temp_array = []
     last_arg = None
 
     for arg in sys.argv[1:]:
         last_arg = arg
 
         if arg == '@':
-            numbers.append(temp)
-            temp = []
+            times.append(temp_array)
+            temp_array = []
         else:
-            temp.append(int(arg))
+            temp_array.append(int(arg))
 
-    if temp:
-        numbers.append(temp)
+    if temp_array:
+        times.append(temp_array)
 
     if last_arg == '@':
-        numbers.append([])
+        times.append([])
+
+    return times
+
+
+def read_times_to_critical_section(input_times):
+    output_times = []
+
+    for j in range(1, len(input_times)):
+        output_times.append(input_times[j])
+
+    return output_times
+
+
+if __name__ == "__main__":
+
+    process_times = read_input_arguments()
+    initial_clock_times = process_times[0]
+
+    initial_clock_times[0] = 8
+
+    times_to_critical_section = read_times_to_critical_section(process_times)
 
     processes = []
 
-    for i, process_init_value in enumerate(numbers[0]):
-        processes.append(WorkerProcess(base_port + i, numbers[0][i], numbers[i + 1]))
+    for i, process_init_value in enumerate(initial_clock_times):
+
+        processes.append(
+            WorkerProcess(
+                id_process=i + 1,
+                clock=process_init_value,
+                times_to_critical_section=times_to_critical_section[i],
+                number_of_other_processes=len(initial_clock_times) - 1,
+            )
+        )
 
     for process in processes:
         process.join()
